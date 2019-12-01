e32d876b92b6a0391500e41dc7ad665a4971d926
boriscetvertakov <cetvertakov@gmail.com>
2019-12-01 13:02:18 +0200

    refactoring calc

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def summa(self, x, y):
+        print("Summa:", x + y)
+
+    def minus(self, x, y):
+        print("Minus", x - y)
+
+    def umnojiti(self, x, y):
+        print("Umnojiti", x * y)
+
+    def deliti(self, x, y):
+        print("Delenie", x / y)
