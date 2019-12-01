b6cc4fa05a158b5a6c73ff75843db2327a8a2163
simionpolivencu <letrock2004@gmail.com>
2019-12-01 13:50:44 +0300

    Added calculator

--- StartApp.py
+++ StartApp.py
@@ -1,1 +1,17 @@
 print("Hello world")
+def calc(*args) :
+    if(args[2]=='+'):
+        a = args[0]+args[1]
+    elif(args[2]=='-'):
+        a=args[0]-args[1]
+    elif(args[2] == '*'):
+        a= args[0]*args[1]
+    elif(args[2]== '/'):
+        a=args[0]/args[1]
+    else:
+        a = str(args[0])+str(args[1])
+    return a
+d = int(input('A'))
+b = int(input('B'))
+c = input('Cno sdelati')
+print(calc(d,b,c))
